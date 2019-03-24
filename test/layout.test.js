1f7e72ab5519d4daac6aae5ebb11806e551292e9
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-03-24 10:58:47 +0700

    chore(test): rect

--- test/layout.test.js
+++ test/layout.test.js
@@ -1,5 +1,5 @@
-const sum = require('../lib/layout');
+const { Rect } = require('../lib/layout');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('rect', () => {
+  expect(new Rect(1, 2, 3, 4).height).toBe(4);
 });
