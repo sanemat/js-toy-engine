a0a32ab602eeb8a7e16b2b364d9b9b1724e9f3e9
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-03-24 13:18:27 +0700

    chore(structure): apply prettier

--- test/layout.test.js
+++ test/layout.test.js
@@ -1,9 +1,9 @@
-const { Rect, LayoutBox, BoxType } = require('../lib/layout');
+const { Rect, LayoutBox, BoxType } = require("../lib/layout");
 
-test('rect', () => {
+test("rect", () => {
   expect(new Rect(1, 2, 3, 4).height).toBe(4);
 });
 
-test('layout box', () => {
+test("layout box", () => {
   expect(LayoutBox.create(BoxType.BlockNode)).toBeInstanceOf(LayoutBox);
 });
