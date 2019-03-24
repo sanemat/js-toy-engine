1a164581fe3eb6ee59054887078b3a2dd5e6193b
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-03-24 12:49:02 +0700

    chore(layout): use box node

--- test/layout.test.js
+++ test/layout.test.js
@@ -1,9 +1,9 @@
-const { Rect, LayoutBox } = require('../lib/layout');
+const { Rect, LayoutBox, BoxType } = require('../lib/layout');
 
 test('rect', () => {
   expect(new Rect(1, 2, 3, 4).height).toBe(4);
 });
 
 test('layout box', () => {
-  expect(LayoutBox.create('blockNode')).toBeInstanceOf(LayoutBox);
+  expect(LayoutBox.create(BoxType.BlockNode)).toBeInstanceOf(LayoutBox);
 });
