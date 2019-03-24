aadba39d150aab458e8538dd368229b61a0c0ba3
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-03-24 16:04:26 +0700

    feat(layout): rect expanded by edge sizes

--- test/layout.test.js
+++ test/layout.test.js
@@ -1,4 +1,4 @@
-const { Rect, LayoutBox, BoxType } = require("../lib/layout");
+const { Rect, LayoutBox, BoxType, EdgeSizes } = require("../lib/layout");
 
 test("rect", () => {
   expect(new Rect(1, 2, 3, 4).height).toBe(4);
@@ -7,5 +7,11 @@ test("rect", () => {
 test("layout box", () => {
   expect(LayoutBox.create(new BoxType.BlockNode(["props"]))).toBeInstanceOf(
     LayoutBox
+  );
+});
+
+test("expandedBy", () => {
+  expect(new Rect(12, 13, 4, 5).expandedBy(new EdgeSizes(1, 2, 3, 4))).toEqual(
+    new Rect(11, 10, 7, 12)
   );
 });
